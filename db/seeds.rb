a8196688d2024864a99800a77eef909cc384436b
kazuhiro-f <kz7260wanderer@gmail.com>
2020-08-21 22:06:58 +0900

    Complete users model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# メインのサンプルユーザを一人作成する
+User.create!(name: "Default Admin", password: "kougakusai2020", password_confirmation: "kougakusai2020", admin: true)
