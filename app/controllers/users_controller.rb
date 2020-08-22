3cae84549ae7bc8dec1e4d8f917afd4e1ed4792c
kazuhiro <kz7260wanderer@gmail.com>
2020-08-22 19:05:40 +0900

    Remove small bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, onlu: :destroy
+  before_action :admin_user, only: :destroy
 
   def index
     @users =User.paginate(page: params[:page])
@@ -58,7 +58,7 @@ class UsersController < ApplicationController
     def logged_in_user
       unless logged_in?
         store_location
-        flahs[:danger] = "Please log in."
+        flash[:danger] = "Please log in."
         redirect_to login_url
       end
     end
