5b08aba7f584d0190991589363617138a6a7a937
kazuhiro <kz7260wanderer@gmail.com>
2020-08-24 10:17:35 +0900

    Pre-release

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
-	before_action :logged_in_user, only: [:index, :new, :create, :destroy]
-	before_action :correct_user, only: :destroy
+	before_action :logged_in_user
+	before_action :correct_user, only: [:edit, :destroy]
+	before_action :admin_user, only: [:create, :new, :edit, :destroy]
 
 	def index
 		@articles = Article.paginate(page: params[:page])
@@ -9,7 +10,7 @@ class ArticlesController < ApplicationController
 	def create
 		@article = current_user.articles.build(article_params)
 		if @article.save
-			flash[:success] = "Article created!"
+			flash[:success] = "記事を投稿しました。"
 			redirect_to root_url
 		else
 			@feed_items = current_user.feed.paginate(page: params[:page])
@@ -28,13 +29,23 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:success] = "記事を更新しました。"
+			redirect_to root_url
+		else
+			render 'edit'
+		end
+	end
+
 	def show
 		@article = Article.find(params[:id])
 	end
 
 	def destroy
 		@article.destroy
-		flash[:success] = "Article deleted"
+		flash[:success] = "記事を削除しました。"
 		redirect_to request.referrer || roor_url
 	end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user
   before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: :destroy
+  before_action :admin_user, only: [:new, :create, :destroy]
 
   def index
     @users =User.paginate(page: params[:page])
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:success] = "Profile updated"
+      flash[:success] = "ユーザのプロフィールを更新しました。"
       redirect_to @user
     else
       render 'edit'
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
 
   def destroy
     User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
+    flash[:success] = "ユーザを削除しました。"
     redirect_to users_url
   end
 
@@ -58,10 +58,5 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
-    end
-
-    # 管理者かどうか確認
-    def admin_user
-      redirect_to(root_url) unless current_user.admin?
     end
 end
