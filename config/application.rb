8232043bd83c4121a33af12a57f694b3a498f652
kazuhiro <kz7260wanderer@gmail.com>
2020-08-28 13:46:44 +0900

    Complete error localization

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,8 @@ module HpBlog
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 		config.time_zone = 'Tokyo'
+		config.i18n.default_locale = :ja
+		config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+		config.add_autoload_paths_to_load_path = false
   end
 end
