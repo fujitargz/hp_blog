e3a96c1f0621828e36c71fcb5fc228c29b14652d
kazuhiro-f <kz7260wanderer@gmail.com>
2021-05-22 13:18:56 +0900

    [update] full title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   #ページごとの完全なタイトルを返す
   def full_title(page_title = '')
-    base_title = "こうがく祭HP村通信"
+    base_title = "開発村通信"
     if page_title.empty?
       base_title
     else
